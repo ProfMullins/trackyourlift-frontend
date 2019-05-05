5ef8f1a4b29b90957bc1adbb1425c30696091cbe
Nicholas Mullins <mullinsprofessor@gmail.com>
2019-05-05 11:04:27 -0400

    feat(setup): Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Track</span>
+        <span class="font-weight-light">YourLift</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!--<span class="mr-2">Latest Release</span>-->
+      <v-btn>
+        <router-link tag="span" class="mr-2" to="/">home</router-link>
+      </v-btn>
+      <v-btn>
+        <router-link tag="span" class="mr-2" to="about">about</router-link>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <!--<Home/>-->
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import Home from './components/Home'
+import About from './components/About'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+  name: 'App',
+  components: {
+    Home,
+    About
+  },
+  data () {
+    return {
+      //
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ src/components/About.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/trackyourlift_logo.png')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-layout>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      whatsNext: [
+        {
+          text: 'Getting Started',
+          href: 'https://example.com'
+        },
+        {
+          text: 'Tips',
+          href: 'https://example.com'
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://example.com'
+        }
+
+      ]
+    })
+  }
+</script>
+
+<style>
+
+</style>
